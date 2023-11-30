d55c19a2ac73f050aed806bc89a709b67ce8c078
liqianxi <qianxi@ualberta.ca>
2023-11-30 10:16:15 -0800

    test

--- network/test.py
+++ network/test.py
@@ -1,39 +1,14 @@
-import metaworld
-import random
+
+from gymnasium.spaces import Box
+import numpy as np
+
+
+from metaworld import MT10
+# import numpy as np
+# import random
 import jax
 import jax.numpy as jnp
-import flax.linen as nn
-import numpy as np
-import optax
-from flax.linen.initializers import constant, orthogonal
-from typing import Sequence, NamedTuple, Any
-from flax.training.train_state import TrainState
-import distrax
-import gymnax
-
-mt10 = metaworld.MT10() # Construct the benchmark, sampling tasks
-
-training_envs = []
-for name, env_cls in mt10.train_classes.items():
-  env = env_cls()
-  task = random.choice([task for task in mt10.train_tasks
-                        if task.env_name == name])
-  env.set_task(task)
-  training_envs.append(env)
-
-# for env in training_envs:
-#   obs = env.reset()  # Reset environment
-#   a = env.action_space.sample()  # Sample an action
-#   obs, reward, terminated, truncated, info = env.step(a)  # Step the environment with the sampled random action
-# import jax
-# import jax.numpy as jnp
 # import flax.linen as nn
-# import numpy as np
-# import optax
-# from flax.linen.initializers import constant, orthogonal
-# from typing import Sequence, NamedTuple, Any
-# from flax.training.train_state import TrainState
-# import distrax
 from wrappers import (
     LogWrapper,
     BraxGymnaxWrapper,
@@ -42,9 +17,51 @@ from wrappers import (
     NormalizeVecReward,
     ClipAction,
 )
+# import optax
+# from flax.linen.initializers import constant, orthogonal
+# from typing import Sequence, NamedTuple, Any
+# from flax.training.train_state import TrainState
+# import distrax
+# import gymnax
+def make_train(config):
+    mt10 = MT10() # Construct the benchmark, sampling tasks
 
-env, env_params = BraxGymnaxWrapper(mt10.train_classes["pick-place-v2"]), None
-env = LogWrapper(env)
-env = ClipAction(env)
-env = VecEnv(env)
 
+
+    env = mt10.train_classes["pick-place-v2"]
+    env, env_params = BraxGymnaxWrapper(env), None
+
+    
+    # env = LogWrapper(env)
+    # env = ClipAction(env)
+    #env = VecEnv(env)
+    # batched_seeds = jnp.array([1, 2, 3, 4])
+    # batched_options = jnp.array([{'param': 0}, {'param': 1}, {'param': 2}, {'param': 3}])
+    # obs = env.reset(batched_seeds,batched_options)  # Reset environment
+    # a = env.action_space.sample()  # Sample an action
+    # obs, reward, terminated, truncated, info = env.step(a)  # Step the environment with the sampled random action
+
+    # print(obs, reward, terminated, truncated, info)
+if __name__ == "__main__":
+    config = {
+        "LR": 3e-4,
+        "NUM_ENVS": 2048,
+        "NUM_STEPS": 10,
+        "TOTAL_TIMESTEPS": 5e7,
+        "UPDATE_EPOCHS": 4,
+        "NUM_MINIBATCHES": 32,
+        "GAMMA": 0.99,
+        "GAE_LAMBDA": 0.95,
+        "CLIP_EPS": 0.2,
+        "ENT_COEF": 0.0,
+        "VF_COEF": 0.5,
+        "MAX_GRAD_NORM": 0.5,
+        "ACTIVATION": "tanh",
+        "ENV_NAME": "hopper",
+        "ANNEAL_LR": False,
+        "NORMALIZE_ENV": True,
+        "DEBUG": True,
+    }
+    rng = jax.random.PRNGKey(30)
+    train_jit = jax.jit(make_train(config))
+    out = train_jit(rng)
--- network/wrappers.py
+++ network/wrappers.py
@@ -6,7 +6,7 @@ from flax import struct
 from functools import partial
 from typing import Optional, Tuple, Union, Any
 from gymnax.environments import environment, spaces
-from brax import envs
+# from brax import envs
 from brax.envs.wrappers.training import EpisodeWrapper, AutoResetWrapper
 
 
@@ -114,35 +114,43 @@ class LogWrapper(GymnaxWrapper):
 
 
 class BraxGymnaxWrapper:
-    def __init__(self, env_name, backend="positional"):
-        env = envs.get_environment(env_name=env_name, backend=backend)
-        env = EpisodeWrapper(env, episode_length=1000, action_repeat=1)
+    def __init__(self, env):
+        #env = envs.get_environment(env_name=env_name, backend=backend)
+        env = EpisodeWrapper(env, episode_length=200, action_repeat=1)
         env = AutoResetWrapper(env)
         self._env = env
-        self.action_size = env.action_size
-        self.observation_size = (env.observation_size,)
+        #self.action_size = env.action_size
+        #self.observation_size = (env.observation_size,)
+        #self.reset = jax.vmap(self._env.reset, in_axes=(0, None))
+        #self.step = jax.vmap(self._env.step, in_axes=(0, 0, 0, None))
 
     def reset(self, key, params=None):
-        state = self._env.reset(key)
+        state = jax.vmap(self._env.reset,(0,0))(key,params)
         return state.obs, state
 
-    def step(self, key, state, action, params=None):
-        next_state = self._env.step(state, action)
-        return next_state.obs, next_state, next_state.reward, next_state.done > 0.5, {}
+    def step(self, action, params=None):
+        obs, reward, terminated, truncated, info = jax.vmap(self._env.step)(action)
+        return obs, reward, terminated, truncated, info
 
-    def observation_space(self, params):
-        return spaces.Box(
-            low=-jnp.inf,
-            high=jnp.inf,
-            shape=(self._env.observation_size,),
-        )
+    def observation_space(self):
+        return self._env.observation_space
 
-    def action_space(self, params):
-        return spaces.Box(
-            low=-1.0,
-            high=1.0,
-            shape=(self._env.action_size,),
-        )
+    def action_space(self):
+        return self._env.action_space
+
+    # def observation_space(self, params):
+    #     return spaces.Box(
+    #         low=-jnp.inf,
+    #         high=jnp.inf,
+    #         shape=(self._env.observation_size,),
+    #     )
+
+    # def action_space(self, params):
+    #     return spaces.Box(
+    #         low=-1.0,
+    #         high=1.0,
+    #         shape=(self._env.action_size,),
+    #     )
 
 
 class ClipAction(GymnaxWrapper):
@@ -185,6 +193,7 @@ class TransformReward(GymnaxWrapper):
 class VecEnv(GymnaxWrapper):
     def __init__(self, env):
         super().__init__(env)
+
         self.reset = jax.vmap(self._env.reset, in_axes=(0, None))
         self.step = jax.vmap(self._env.step, in_axes=(0, 0, 0, None))
 
